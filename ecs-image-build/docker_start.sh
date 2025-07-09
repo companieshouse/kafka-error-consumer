396267fff6f49e7c319ed202e94577c55bef4a44
drobledomunoz-ch <drobledomunoz@companieshouse.gov.uk>
2025-07-09 10:59:54 +0100

    set tech-debt changes for kafka-error-consumer

--- ecs-image-build/docker_start.sh
+++ ecs-image-build/docker_start.sh
@@ -3,4 +3,4 @@
 # Start script for kafka-error-consumer
 
 PORT=8080
-exec java -jar -Dserver.port="${PORT}" "kafka-error-consumer.jar"
+exec java -jar -Dserver.port="${PORT}" -XX:MaxRAMPercentage=80 "kafka-error-consumer.jar"
