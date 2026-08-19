4952fa98e3a44a9299c94dd96fd2a7571e79eff8
Christopher Gregory <84070107+cgregoryCH@users.noreply.github.com>
2026-08-19 16:35:35 +0100

    Revert "Makes idempotence configurable for use on Kafka 0.10 temporarily"

--- src/main/java/uk/co/companieshouse/kafka/error/config/KafkaProducerConfig.java
+++ src/main/java/uk/co/companieshouse/kafka/error/config/KafkaProducerConfig.java
@@ -24,11 +24,17 @@ public class KafkaProducerConfig {
     @Bean
     public ProducerFactory<String, byte[]> producerFactory() {
         Map<String, Object> configProps = new HashMap<>();
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaErrorProperties.getBootstrapAddress());
-        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, ByteArraySerializer.class);
-        configProps.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, kafkaErrorProperties.getEnableIdempotence());
-        return new DefaultKafkaProducerFactory<>(configProps, new StringSerializer(), new ByteArraySerializer());
+        configProps.put(
+                ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                kafkaErrorProperties.getBootstrapAddress());
+        configProps.put(
+                ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+                StringSerializer.class);
+        configProps.put(
+                ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+                ByteArraySerializer.class);
+        return new DefaultKafkaProducerFactory(configProps, new StringSerializer(),
+                new ByteArraySerializer());
     }
 
     @Bean
